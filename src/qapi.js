43e7dd33bc74637fe15b3f1fae0bbb082260097f
fedkozoro <74133467+fedkozoro@users.noreply.github.com>
2020-11-08 19:14:33 +1000

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fa7af6a2c4ef2001e31e535",
+				"5fa7af6b2c4ef2001e31e537"
+			]
+		},
+		"5fa7af6a2c4ef2001e31e535": {
+			"id": "5fa7af6a2c4ef2001e31e535",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fa7af6b2c4ef2001e31e537": {
+			"id": "5fa7af6b2c4ef2001e31e537",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
